fe05e0557e0309aaa932b1886c2fe9fc683234e3
Mihai Neacsu <mihai.mneacsu@gmail.com>
2016-07-26 12:29:00 +0100

    Add 'hello, world' running

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('ok')
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8000, function () {
+  console.log('App listening on port 8000!');
+});
